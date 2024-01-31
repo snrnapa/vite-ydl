773677f7ae97b10f61bff8f490fbccf482a6b967
snrnapa <shino.satoru@gmail.com>
2024-02-01 00:51:26 +0900

    バックのpythonより、ファイル情報を取得するfetchを実装した

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/file_info')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // const list = data.message;
+        // setBreeds(Object.keys(list));
+      });
+  }, []);
   return (
     <>
       <h1 className="bg-teal-400">Vite + React</h1>
